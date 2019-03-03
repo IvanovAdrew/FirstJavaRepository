6185576e201862c36827a91f028b6c2e0951b937
IvanovAdrew <zyablik7771@gmail.com>
2019-03-03 16:42:16 +0200

    equals end

--- src/basic/world/MethodEquals/Car.java
+++ src/basic/world/MethodEquals/Car.java
@@ -3,10 +3,21 @@ package basic.world.MethodEquals;
 public class Car {
     String unicNumber;
     String colorOfCar;
+    public Car (String unicNumber,String colorOfCar){
+        this.unicNumber=unicNumber;
+        this.colorOfCar=colorOfCar;
+    }
 
     @Override
     public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-        return false;
+        Car car = (Car) o;
+        return unicNumber!=null ? unicNumber.equals(car.unicNumber) : car.unicNumber == null;
+
+    }
+    public String sayingSomething (){
+        return("my color is " + colorOfCar);
     }
 }
---
+++ src/basic/world/MethodEquals/Main.java
@@ -0,0 +1,34 @@
+package basic.world.MethodEquals;
+
+public class Main {
+    public static void main (String args[]){
+        Car firstCar = new Car("UA7896KA","green");
+        Car secondCar = new Car("UA7896KA","blue");
+        Car thirdCar = new Car("UA7896KA","black");
+        Car fourthCar = new Car("PI2345KI","black");
+        Car fifthCar = new Car("KI7490OJ","white");
+        System.out.println("firstCar equals with secondCar " + firstCar.equals(firstCar)); //reflexive
+        System.out.println();
+
+        System.out.println("firstCar equals with secondCar " + firstCar.equals(secondCar));
+        System.out.println("secondCar equals with firstCar " + firstCar.equals(secondCar));//symmetric
+        System.out.println();
+
+        System.out.println("firstCar equals with secondCar " + firstCar.equals(secondCar)); //transitive
+        System.out.println("secondCar equals with thirdCar " + secondCar.equals(thirdCar));
+        System.out.println("firstCar equals with thirdCar " + firstCar.equals(thirdCar));
+        System.out.println();
+
+        System.out.println("firstCar equals with secondCar for many time... " + firstCar.equals(secondCar)); //consistent
+        System.out.println("firstCar equals with secondCar " + firstCar.equals(secondCar));
+        System.out.println("firstCar equals with secondCar " + firstCar.equals(secondCar));
+        System.out.println("firstCar equals with secondCar " + firstCar.equals(secondCar));
+        System.out.println("firstCar equals with secondCar " + firstCar.equals(secondCar));
+        System.out.println();
+
+        System.out.println("firstCar is not null "+firstCar.equals(null)); //not null
+
+
+
+    }
+}
