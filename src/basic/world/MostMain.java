15c099b4d6e4eff6c51b544d68c7c74dbdaf2b68
IvanovAdrew <zyablik7771@gmail.com>
2019-03-03 15:13:48 +0200

    third try

--- src/basic/world/MostMain.java
+++ src/basic/world/MostMain.java
@@ -9,6 +9,7 @@ public class MostMain {
     public static void main(String[]args){
         Timer timer = new Timer();
         Timer timerSecondTry = new Timer();
+        Timer timerThirdTry = new Timer();
 
         Scanner in = new Scanner(System.in);
         List<String> story = new ArrayList<String>();
